be29d41688a32b0221be9336ac662ed66b61b752
Sakshi Shah <shahsakshi1702@gmail.com>
2026-06-20 16:12:34 +0530

    update in file

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from devSecOps Project"
+    return "DevSecOps CI/CD Pipeline with Jenkins, Docker, Kubernetes and AWS" \
+    "Project Overview
+
+Designed and implemented an end-to-end DevSecOps CI/CD pipeline for automated application deployment using AWS, Jenkins, Docker, Docker Hub, and Kubernetes. The project demonstrates modern DevOps practices by automating code integration, containerization, image management, and application deployment in a Kubernetes cluster."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
